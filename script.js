caebe2c32ddcb9356281cf8fda13be4c06ad1914
jvp36 <jvp.36@yahoo.com>
2025-01-11 14:14:44 -0400

    Add change status read

--- script.js
+++ script.js
@@ -16,16 +16,46 @@ Book.prototype.info = function() {
         return `${this.title} by ${this.author}, ${this.pages}, not read yet.`
     }
 }
+Book.prototype.changeRead = function(newRead) {
+    this.read = newRead; 
+}
 // add book to myLibrary function
 function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read)
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+// remove book function
+function removeBook(row) {  
+    //console.log(row.id)    
+    myLibrary.splice(row.id, 1);
+    renderTable(myLibrary);
+    if(myLibrary.length === 0) {
+        $table.style.display = "none";
+    }
+    //console.log(myLibrary);    
+}
+// edit book function
+
+function changeReadStatus(e) {
+    let i = e.target.id;
+    //console.log(myLibrary[i]);
+    //console.log(myLibrary[i]);
+    if(myLibrary[i].read === true) {
+        myLibrary[i].read = false;
+    } else {
+        myLibrary[i].read = true;
+    }
+    //console.log(myLibrary[i]);
+    renderTable(myLibrary);    
+}
+
 // render table function
 function renderTable(array) {
-    $tbody.innerHTML = "";
+    $tbody.innerHTML = "";    
+    let pos = 0;
     array.forEach(element => {
-        const $row = document.createElement("tr");  
+        const $row = document.createElement("tr"); 
+        $row.setAttribute("id", pos);
         const $title = document.createElement("td");
         $title.textContent = element.title;
         const $autor = document.createElement("td");
@@ -42,16 +72,26 @@ function renderTable(array) {
         const $edit = document.createElement("td");
         const $editBtn = document.createElement("button");
         $editBtn.classList.add("btn");
-        $editBtn.textContent = "Edit";
+        $editBtn.setAttribute("id", pos);
+        pos++; 
+        $editBtn.textContent = "Change Status Read";
         $edit.appendChild($editBtn);
         $row.append($title, $autor, $pages, $info, $delete, $edit);
         $tbody.appendChild($row);
+        $delBtn.addEventListener("click", () => removeBook($row));
+        $editBtn.addEventListener("click", changeReadStatus);                                 
     });
-    $table.style.display = "block";    
+    $table.style.display = "block";
 }
+// display new book form
 function displayFormNewBook() {
     $formNewBook.style.display = "block";
 }
+// display edit book form
+function displayEditform() {
+    $editBookForm.style.display = "block";
+}
+
 // DOM variables
 const $addNewBook = document.getElementById("new-book");
 const $formNewBook = document.getElementById("form-new");
@@ -60,6 +100,7 @@ const $inputAuthor = document.getElementById("author");
 const $inputPages = document.getElementById("pages");
 const $inputRead = document.getElementById("read");
 const $saveNewBook = document.getElementById("save");
+const $editBookForm = document.getElementById("form-edit");
 const $table = document.getElementById("table");
 const $tbody = document.getElementById("tbody");
 // DOM events
@@ -74,8 +115,11 @@ $saveNewBook.addEventListener("click", (e) => {
     renderTable(myLibrary);
     $formNewBook.reset();
     $formNewBook.style.display ="none";
-    console.log(myLibrary); 
+    //console.log(myLibrary); 
 })
+
+
+
 
 
 //addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, true);
